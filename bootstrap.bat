a8cd1a28121aba751f64588863191f1bb83827fa
Stefano Famà <8192713+famastefano@users.noreply.github.com>
2025-09-28 11:35:56 +0200

    Created minimal script to build the build-system

---
+++ bootstrap.bat
@@ -0,0 +1,10 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat"
+
+:: Test Framework
+cl @bootstrap.cl /c /I source/shared/test_framework/public /I source/shared/test_framework/private source/shared/test_framework/private/handler.cpp
+
+:: JSON
+:: cl @bootstrap.cl /c /I source/shared/test_framework/public source/shared/json/test/json_test.cpp
+
+::   Runner
+:: cl @bootstrap.cl /Fe: "build/bootstrap/exe/json" /I source/shared/test_framework/public /I source/shared/test_framework/private source/shared/test_framework/private/main.cpp build/bootstrap/obj/test_framework.obj build/bootstrap/obj/json_test.obj /link /SUBSYSTEM:console /DEBUG /PDB:"build/bootstrap/exe/json.pdb"
